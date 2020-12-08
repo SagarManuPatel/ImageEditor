541f7d2a03b9757e09145cae0eef591f68ef5a3d
Sagar Patel <sagar.patel@myglamm.com>
2020-12-08 21:00:55 +0530

    Bug Fixed

--- ImageEditor/Controllers/Camera/View/ColorSelectCell.swift
+++ ImageEditor/Controllers/Camera/View/ColorSelectCell.swift
@@ -11,20 +11,35 @@ import UIKit
 
 class ColorSelectCell : BaseCell {
     
+    var selectedCell : Bool? {
+        didSet{
+            containerViewHeightAnchor?.constant = selectedCell ?? false ? 50 : 40
+            containerViewWidthAnchor?.constant = selectedCell ?? false ? 50 : 40
+            containerView.layer.cornerRadius = selectedCell ?? false ? 25 : 20
+        }
+    }
+    
     let containerView : UIView = {
         let c = UIView()
         c.translatesAutoresizingMaskIntoConstraints = false
         c.layer.cornerRadius = 20
+        c.layer.borderWidth = 0.5
+        c.layer.backgroundColor = UIColor.black.cgColor
         return c
     }()
+    
+    var containerViewHeightAnchor : NSLayoutConstraint?
+    var containerViewWidthAnchor : NSLayoutConstraint?
     
     override func addCustomCellViews() {
         addSubview(containerView)
         
         containerView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         containerView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        containerView.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        containerView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        containerViewHeightAnchor = containerView.heightAnchor.constraint(equalToConstant: 40)
+            containerViewHeightAnchor?.isActive = true
+        containerViewWidthAnchor = containerView.widthAnchor.constraint(equalToConstant: 40)
+            containerViewWidthAnchor?.isActive = true
     }
     
 }
--- ImageEditor/Controllers/Camera/View/PreviewPhotoContainerView.swift
+++ ImageEditor/Controllers/Camera/View/PreviewPhotoContainerView.swift
@@ -15,6 +15,12 @@ class PreviewPhotoContainerView: BaseClass {
     
     var colorsArray: [UIColor] = [#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1), #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), #colorLiteral(red: 1, green: 0.4059419876, blue: 0.2455089305, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1), #colorLiteral(red: 1, green: 0.4059419876, blue: 0.2455089305, alpha: 1), #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1), #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1), #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), #colorLiteral(red: 0.3176470697, green: 0.07450980693, blue: 0.02745098062, alpha: 1), #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), #colorLiteral(red: 0.3823936913, green: 0.8900789089, blue: 1, alpha: 1), #colorLiteral(red: 1, green: 0.4528176247, blue: 0.4432695911, alpha: 1), #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1), #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)]
     
+    var selectedColorIndex : Int? {
+        didSet{
+            bottomCollectionView.reloadData()
+        }
+    }
+    
     let previewImageView: UIImageView = {
         let iv = UIImageView()
         return iv
@@ -40,6 +46,7 @@ class PreviewPhotoContainerView: BaseClass {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .white
         cv.delegate = self
         cv.dataSource = self
         return cv
@@ -100,6 +107,7 @@ extension PreviewPhotoContainerView : UICollectionViewDelegate , UICollectionVie
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! ColorSelectCell
+        cell.selectedCell = indexPath.item == selectedColorIndex ? true : false
         cell.containerView.backgroundColor = colorsArray[indexPath.item]
         return cell
     }
@@ -117,6 +125,7 @@ extension PreviewPhotoContainerView : UICollectionViewDelegate , UICollectionVie
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.selectedColorIndex = indexPath.item
         canvas.setStrokeColor(color: colorsArray[indexPath.item])
     }
 }
